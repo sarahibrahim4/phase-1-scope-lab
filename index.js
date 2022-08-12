e0ae7ae8bbd5325de5e9db6c6c9fc74ceff3573d
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-12 21:16:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+console.log(customerName);
+
+function upperCaseCustomerName(){
+return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+return bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer(){
+return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bilal";
+
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = "ibrahim";
+return leastFavoriteCustomer;
+}
